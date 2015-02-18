f23d05fd3d3357831cfb45e0308e295f3f9e5db2
jmartin <jmartin@zikzakmedia.com>
2015-02-18 16:49:31 +0100

    Refactored code in order to improve its performance

--- move.py
+++ move.py
@@ -21,60 +21,93 @@ class Move:
         Lot = pool.get('stock.lot')
         Date = pool.get('ir.date')
         Configuration = pool.get('stock.configuration')
+        cursor = Transaction().cursor
 
         configuration = Configuration(1)
         lot_priority = configuration.lot_priority or 'lot_date'
 
         today = Date.today()
         new_moves = []
-        lots_by_product = {}
-        consumed_quantities = {}
+        to_update = []
+        product_ids = [m.product.id for m in moves]
+        location_ids = [m.from_location.id for m in moves]
+        ctx = {
+            'stock_date_end': today,
+            'stock_assign': True,
+            'forecast': False,
+            'locations': location_ids,
+            }
+        with Transaction().set_context(ctx):
+            lots = Lot.search([
+                    ('product', 'in', product_ids),
+                    ('quantity', '>', 0.0),
+                    ], order=[(lot_priority, 'ASC')])
+        query = cls.compute_quantities_query(location_ids)
+        cursor.execute(*query)
+        quantities = cursor.fetchall()
+        lots_by_product = {
+            (q[0], q[1]): [[l, l.quantity]
+                for l in lots
+                if q[1] == l.product.id]
+            for q in quantities if q[2] > 0.0 and q[1] in product_ids
+            }
+
         for move in moves:
             if (not move.lot and move.product.lot_is_required(
                         move.from_location, move.to_location)):
-                if not move.product.id in lots_by_product:
-                    search_context = {
-                        'stock_date_end': today,
-                        'locations': [move.from_location.id],
-                        'stock_assign': True,
-                        'forecast': False,
-                        }
-                    with Transaction().set_context(search_context):
-                        lots_by_product[move.product.id] = Lot.search([
-                                ('product', '=', move.product.id),
-                                ('quantity', '>', 0.0),
-                                ], order=[(lot_priority, 'ASC')])
-
-                lots = lots_by_product[move.product.id]
+                location = move.from_location
+                product = move.product
                 remainder = move.internal_quantity
+                lots = lots_by_product.get((location.id, product.id), False)
                 while lots and remainder > 0.0:
-                    lot = lots.pop(0)
-                    consumed_quantities.setdefault(lot.id, 0.0)
-                    lot_quantity = lot.quantity - consumed_quantities[lot.id]
+                    lot, lot_quantity = lots[0]
                     assigned_quantity = min(lot_quantity, remainder)
+                    quantity = Uom.compute_qty(
+                        move.product.default_uom, assigned_quantity,
+                        move.uom)
                     if assigned_quantity == remainder:
-                        move.quantity = Uom.compute_qty(
-                            move.product.default_uom, assigned_quantity,
-                            move.uom)
-                        move.lot = lot
-                        move.save()
-                        lots.insert(0, lot)
+                        values = {
+                            'quantity': quantity,
+                            'lot': lot,
+                            }
+                        to_update.extend(([move], values))
+                        lots[0][1] -= assigned_quantity
                     else:
-                        quantity = Uom.compute_qty(
-                            move.product.default_uom, assigned_quantity,
-                            move.uom)
-                        new_moves.extend(cls.copy([move], {
-                                    'lot': lot.id,
-                                    'quantity': quantity,
-                                    }))
+                        values = {
+                            name: getattr(move, name)
+                                if (getattr(getattr(cls, name), '_type', False
+                                    ) not in ('reference',
+                                        'many2one', 'one2many', 'many2many'))
+                                else
+                                    getattr(getattr(move, name), 'id', None)
+                                if (getattr(getattr(cls, name), '_type', False
+                                    ) != 'reference')
+                                else
+                                    '%s,%s' % (getattr(getattr(move, name),
+                                        '__name__'),
+                                        getattr(getattr(move, name), 'id'))
+                                for name in cls._fields
+                                if getattr(move, name, False)
+                                    and name != 'id'
+                                    and (getattr(getattr(cls, name), '_type',
+                                            False) != 'function')
+                            }
+                        values['quantity'] = quantity
+                        values['lot'] = lot
+                        new_moves.append(values)
+                        lots.pop(0)
 
-                    consumed_quantities[lot.id] += assigned_quantity
                     remainder -= assigned_quantity
                 if not lots:
                     move.quantity = Uom.compute_qty(move.product.default_uom,
                         remainder, move.uom)
                     move.save()
                 lots_by_product[move.product.id] = lots
+
+        if to_update:
+            cls.write(*to_update)
+        if new_moves:
+            new_moves = cls.create(new_moves)
 
         return super(Move, cls).assign_try(new_moves + moves,
             with_childs=with_childs, grouping=grouping)
