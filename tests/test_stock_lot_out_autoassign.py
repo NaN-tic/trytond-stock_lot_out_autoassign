d8e6449a4feac4ecc87d35088dac3289dfadcd0c
resteve <resteve@zikzakmedia.com>
2016-03-30 10:42:48 +0200

    Reactive RST tests scenarios

--- tests/test_stock_lot_out_autoassign.py
+++ tests/test_stock_lot_out_autoassign.py
@@ -6,6 +6,7 @@ import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
 class StockLotOutAutoassignTestCase(ModuleTestCase):
@@ -19,5 +20,6 @@ def suite():
         StockLotOutAutoassignTestCase))
     suite.addTests(doctest.DocFileSuite('scenario_stock_shipment_out.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
